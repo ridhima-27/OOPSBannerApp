622b9b8ef1ea03f5ea92d9b2d785670e8e21ec70
ridhima-27 <rs4357@srmist.edu.in>
2026-03-03 09:42:35 +0530

    UC5

---
+++ OOPSBannerAppUC5.java
@@ -0,0 +1,52 @@
+public class OOPSBannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            // Row 1
+            String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "
+            ),
+
+            // Row 2
+            String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "
+            ),
+
+            // Row 3
+            String.join("   ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** "
+            ),
+
+            // Row 4
+            String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "     * "
+            ),
+
+            // Row 5
+            String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** "
+            )
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
