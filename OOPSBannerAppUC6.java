03416f51a17a24ef626257e49609b5af347f5c50
ridhima-27 <rs4357@srmist.edu.in>
2026-03-10 08:58:25 +0530

    UC6

---
+++ OOPSBannerAppUC6.java
@@ -0,0 +1,52 @@
+public class OOPSBannerAppUC6 {
+
+    // O Pattern
+    public static String[] getOPattern() {
+        return new String[]{
+                "  ***  ",
+                " *   * ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " *   * ",
+                "  ***  "
+        };
+    }
+
+    // P Pattern
+    public static String[] getPPattern() {
+        return new String[]{
+                "***** ",
+                "*   * ",
+                "*   * ",
+                "***** ",
+                "*     ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    // S Pattern
+    public static String[] getSPattern() {
+        return new String[]{
+                " *****",
+                "*     ",
+                "*     ",
+                " **** ",
+                "     *",
+                "     *",
+                "***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  " + oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
+        }
+    }
+}
